6c2e4dcafa18181da4887fc18e69136e34505ab2
Jack McBride <jlgm2@kent.ac.uk>
2017-05-16 18:56:13 +0100

    obs handler

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from scraper import Scraper
 from location_handler import LocationHandler
-from data_handler import DataHandler
+from observation_handler import ObservationHandler
 
 api_key = "7247916d-91ac-4de7-84e1-088c23dd8432"
 
 def main():
+    #Handler class for locations in the UK
     locationhandler = LocationHandler()
-    datahandler = DataHandler()
+    #Handler class for 24Hr weather observations 
+    obshandler = ObservationHandler()
 
-    datahandler.get_weather_observation_data("Gravesend-Broadness")
+    obshandler.get_weather_observation_data("Gravesend-Broadness")
+    obshandler.print_hourly_data()
 
     return
 
---
+++ observation_handler.py
@@ -0,0 +1,69 @@
+from location_handler import LocationHandler
+import json
+from pprint import pprint
+import urllib
+
+class ObservationHandler(object):
+
+    location_handler = LocationHandler()
+    data = []
+
+    #return a tuple of 24 hour weather data for a given location.
+    '''
+    G (mph) Wind Gust
+    T (C) Temperature
+    V (m) Visibility
+    D (compass) Wind Direction
+    S (mph) Wind Speed
+    W Weather Type
+    P (hpa) Pressure
+    Pt (Pa/s) Pressure Tendency
+    Dp (C) Dew Point
+    H (%) Screen Relative Humidity
+    '''
+    def get_weather_observation_data(self, location):
+        self.location = location
+        location_id = self.location_handler.get_location_id(location)
+
+        #do some error handling for this. Not all locations can be queried for weather observations
+        location_url = "http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/" + location_id[0] + "?res=hourly&key=7247916d-91ac-4de7-84e1-088c23dd8432"
+        #try catch
+
+        response = urllib.urlopen(location_url)
+        json_data = json.load(response)
+        record = json_data['SiteRep']['DV']
+
+        #return 24 hour of weather observation, split into two dates 
+        location_record = record['Location']['Period']
+
+        hourly_data = []
+
+        day_1 = location_record[0]['Rep']
+        day_2 = location_record[1]['Rep']
+
+        for hour in day_1:
+            hourly_data.append(hour)
+
+        for hour in day_2:
+            hourly_data.append(hour)
+
+        for i in range(0, len(hourly_data)):
+            hourly_data[i]['$'] = int(hourly_data[i]['$'])/60
+            hourly_data[i]['$'] = str(hourly_data[i]['$'])
+            #Add 0 digits to maintain length
+            if(len(hourly_data[i]['$']) < 2):
+                hourly_data[i]['$'] = "0" + hourly_data[i]['$'] + ":00"
+            else:
+                hourly_data[i]['$'] = hourly_data[i]['$'] + ":00"
+
+        global data
+        data = hourly_data
+        #Return the 24 hour data
+        return hourly_data
+
+    def print_hourly_data(self):
+        for hour in data:
+            print hour
+
+
+    
